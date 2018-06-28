28be9003270a4e4528fdeb8bb162928bcdb8adfa
malkeet-bst <malkeet@bluestacks.com>
2018-06-28 14:50:29 +0530

    progress in header

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -5,15 +5,28 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     render() {
         return (
-            <div className="container" >
-                <div className="row" >
+            <div className="" >
+                <div className="heading" >
                     <div className="header__left" >
                         <div className="header-item" >
-                            <a className="header-item__content" href="/" >hello
-                                {/* < img alt="MSD" className="logo" src="/app/images/icon.png" /> */}
+                            <a className="header-item-content" href="/" >
+                                < img alt="MSD" className="logo" src="/app/images/icon.png" />
                             </a>
                             <a className="header-item header-item--address"></a>
-                            <div className="header-item__label" > Your Location </div>
+                            <div className="header-item-label" > Your Location </div>
+                            <div className="seacrhContainer">
+                                <input className="searchBox" value="" placeholder="hey" />
+                            </div>
+                            {/* <span className="material-icons searchIcon">search</span> */}
+                            <div className="myAccount">
+                                <span>My Account
+                            </span>
+                                <span className="material-icons expandIcon">expand_more
+                                </span>
+                            </div>
+                            <span className="material-icons cartIcon">local_grocery_store
+                                </span>
+                                <span className="cartValue">$238</span>
                         </div>
                     </div>
                 </div>
