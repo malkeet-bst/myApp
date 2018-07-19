252d6fdc726e99e275afd364cb4e39c53a4a66c4
malkeet-bst <malkeet@bluestacks.com>
2018-07-19 22:59:08 +0530

    showing correct number of items

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -6,195 +6,236 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      selectedColor: "",
-      color1: "white",
-      color2: "white",
-      color3: "white",
-      color4: "white"
+      selectedColor: "#FFB150",
+      correctSequence: ["#FFB150", "#FF5A5F", "#007A87", "#7B0051"],
+      color: ["white", "white", "white", "white"],
+      correctColor: ["white", "white", "white", "white"]
     };
   }
 
+  shuffle = array => {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  };
+
+  checkColors = () => {
+    let correctColor = [];
+    for (var index = 0; index < this.state.correctSequence.length; index++) {
+      if (this.state.correctSequence[index] == this.state.color[index]) {
+        correctColor[index] = "black";
+      } else {
+        correctColor[index] = "white";
+      }
+    }
+    console.log({ correctColor });
+    let shuffledArr = this.shuffle(correctColor);
+    console.log({ shuffledArr });
+    this.setState({ correctColor: shuffledArr });
+  };
   handleClick = choice => {
-    debugger;
     this.setState({ selectedColor: choice });
   };
 
-  fillColor = color => {
-    debugger;
-    this.setState({ [color]: this.state.selectedColor });
+  fillColor = index => {
+    let tempColor = this.state.color;
+    tempColor[index] = this.state.selectedColor;
+    this.setState({ color: tempColor });
   };
   render() {
     return (
-      <body>
+      <div className="main">
         <h1>Master Mind</h1>
-        <section className="main-section">
-          <svg height="44" width="44">
-            <circle
-              onClick={this.fillColor.bind(this, "color1")}
-              cx="22"
-              cy="22"
-              r="18"
-              stroke="black"
-              strokeWidth="1"
-              fill={this.state.color1}
-            />
-          </svg>
+        <div className="parent">
+          <section className="main-section">
+            <svg height="38" width="38">
+              <circle
+                onClick={this.fillColor.bind(this, 0)}
+                cx="19"
+                cy="19"
+                r="15"
+                stroke="black"
+                strokeWidth="1"
+                fill={this.state.color[0]}
+              />
+            </svg>
 
-          <svg height="44" width="44">
-            <circle
-              onClick={this.fillColor.bind(this, "color2")}
-              cx="22"
-              cy="22"
-              r="18"
-              stroke="black"
-              strokeWidth="1"
-              fill={this.state.color2}
-            />
-          </svg>
-          <svg height="44" width="44">
-            <circle
-              onClick={this.fillColor.bind(this, "color3")}
-              cx="22"
-              cy="22"
-              r="18"
-              stroke="black"
-              strokeWidth="1"
-              fill={this.state.color3}
-            />
-          </svg>
-          <svg height="44" width="44">
-            <circle
-              onClick={this.fillColor.bind(this, "color4")}
-              cx="22"
-              cy="22"
-              r="18"
-              stroke="black"
-              strokeWidth="1"
-              fill={this.state.color4}
-            />
-          </svg>
-          <div className="correct-colors">
-            <div>
-              <svg height="22" width="22">
-                <circle
-                  cx="11"
-                  cy="11"
-                  r="10"
-                  stroke="black"
-                  strokeWidth="1"
-                  fill="white"
-                />
-              </svg>
-              <svg height="22" width="22">
-                <circle
-                  cx="11"
-                  cy="11"
-                  r="10"
-                  stroke="black"
-                  strokeWidth="1"
-                  fill="white"
-                />
-              </svg>{" "}
+            <svg height="38" width="38">
+              <circle
+                onClick={this.fillColor.bind(this, 1)}
+                cx="19"
+                cy="19"
+                r="15"
+                stroke="black"
+                strokeWidth="1"
+                fill={this.state.color[1]}
+              />
+            </svg>
+            <svg height="38" width="38">
+              <circle
+                onClick={this.fillColor.bind(this, 2)}
+                cx="19"
+                cy="19"
+                r="15"
+                stroke="black"
+                strokeWidth="1"
+                fill={this.state.color[2]}
+              />
+            </svg>
+            <svg height="38" width="38">
+              <circle
+                onClick={this.fillColor.bind(this, 3)}
+                cx="19"
+                cy="19"
+                r="15"
+                stroke="black"
+                strokeWidth="1"
+                fill={this.state.color[3]}
+              />
+            </svg>
+            <span
+              onClick={this.checkColors}
+              className="material-icons expandIcon"
+            >
+              done
+            </span>
+            <div className="correct-colors">
+              <div>
+                <svg height="18" width="18">
+                  <circle
+                    cx="9"
+                    cy="9"
+                    r="7"
+                    stroke="black"
+                    strokeWidth="1"
+                    fill={this.state.correctColor[0]}
+                  />
+                </svg>
+                <svg height="18" width="18">
+                  <circle
+                    cx="9"
+                    cy="9"
+                    r="7"
+                    stroke="black"
+                    strokeWidth="1"
+                    fill={this.state.correctColor[1]}
+                  />
+                </svg>{" "}
+              </div>
+              <div>
+                <svg height="18" width="18">
+                  <circle
+                    cx="9"
+                    cy="9"
+                    r="7"
+                    stroke="black"
+                    strokeWidth="1"
+                    fill={this.state.correctColor[2]}
+                  />
+                </svg>
+                <svg height="18" width="18">
+                  <circle
+                    cx="9"
+                    cy="9"
+                    r="7"
+                    stroke="black"
+                    strokeWidth="1"
+                    fill={this.state.correctColor[3]}
+                  />
+                </svg>{" "}
+              </div>
             </div>
-            <div>
-              <svg height="22" width="22">
-                <circle
-                  cx="11"
-                  cy="11"
-                  r="10"
-                  stroke="black"
-                  strokeWidth="1"
-                  fill="white"
-                />
-              </svg>
-              <svg height="22" width="22">
-                <circle
-                  cx="11"
-                  cy="11"
-                  r="10"
-                  stroke="black"
-                  strokeWidth="1"
-                  fill="white"
-                />
-              </svg>{" "}
-            </div>
-          </div>
-        </section>
-        <aside className="select-color">
-          <svg height="44" width="44">
-            <circle
-              id="main-color1"
-              onClick={this.handleClick.bind(this, "#FFB180")}
-              cx="22"
-              cy="22"
-              r="18"
-              stroke="black"
-              strokeWidth="1"
-              fill="#FFB180"
-            />
-          </svg>
-          <svg height="44" width="44">
-            <circle
-              id="main-color2"
-              onClick={this.handleClick.bind(this, "#FF5A5F")}
-              cx="22"
-              cy="22"
-              r="18"
-              stroke="black"
-              strokeWidth="1"
-              fill="#FF5A5F"
-            />
-          </svg>
-          <svg height="44" width="44">
-            <circle
-              id="main-color3"
-              onClick={this.handleClick.bind(this, "#8CE071, main-color3")}
-              cx="22"
-              cy="22"
-              r="18"
-              stroke="black"
-              strokeWidth="1"
-              fill="#8CE071"
-            />
-          </svg>
-          <svg height="44" width="44">
-            <circle
-              id="main-color4"
-              onClick={this.handleClick.bind(this, "#00D1C1")}
-              cx="22"
-              cy="22"
-              r="18"
-              stroke="black"
-              strokeWidth="1"
-              fill="#00D1C1"
-            />
-          </svg>
-          <svg height="44" width="44">
-            <circle
-              id="main-color5"
-              onClick={this.handleClick.bind(this, "#007A87")}
-              cx="22"
-              cy="22"
-              r="18"
-              stroke="black"
-              strokeWidth="1"
-              fill="#007A87"
-            />
-          </svg>
-          <svg height="44" width="44">
-            <circle
-              id="main-color6"
-              onClick={this.handleClick.bind(this, "#7B0051")}
-              cx="22"
-              cy="22"
-              r="18"
-              stroke="black"
-              strokeWidth="1"
-              fill="#7B0051"
-            />
-          </svg>
-        </aside>
-      </body>
+          </section>
+          <aside className="select-color">
+            <svg height="38" width="38">
+              <circle
+                id="main-color1"
+                onClick={this.handleClick.bind(this, "#FFB150")}
+                cx="19"
+                cy="19"
+                r="15"
+                stroke="black"
+                strokeWidth="1"
+                fill="#FFB150"
+              />
+            </svg>
+            <svg height="38" width="38">
+              <circle
+                id="main-color2"
+                onClick={this.handleClick.bind(this, "#FF5A5F")}
+                cx="19"
+                cy="19"
+                r="15"
+                stroke="black"
+                strokeWidth="1"
+                fill="#FF5A5F"
+              />
+            </svg>
+            <svg height="38" width="38">
+              <circle
+                id="main-color3"
+                onClick={this.handleClick.bind(this, "#8CE071")}
+                cx="19"
+                cy="19"
+                r="15"
+                stroke="black"
+                strokeWidth="1"
+                fill="#8CE071"
+              />
+            </svg>
+            <svg height="38" width="38">
+              <circle
+                id="main-color4"
+                onClick={this.handleClick.bind(this, "#00D1C1")}
+                cx="19"
+                cy="19"
+                r="15"
+                stroke="black"
+                strokeWidth="1"
+                fill="#00D1C1"
+              />
+            </svg>
+            <svg height="38" width="38">
+              <circle
+                id="main-color5"
+                onClick={this.handleClick.bind(this, "#007A87")}
+                cx="19"
+                cy="19"
+                r="15"
+                stroke="black"
+                strokeWidth="1"
+                fill="#007A87"
+              />
+            </svg>
+            <svg height="38" width="38">
+              <circle
+                id="main-color6"
+                onClick={this.handleClick.bind(this, "#7B0051")}
+                cx="19"
+                cy="19"
+                r="15"
+                stroke="black"
+                strokeWidth="1"
+                fill="#7B0051"
+              />
+            </svg>
+          </aside>
+        </div>
+      </div>
     );
   }
 }
